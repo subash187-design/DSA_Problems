fa02c1f0ca5096d9d82f4330e935115783bbbada
Subash <s8581553@gmail.com>
2026-07-21 09:50:38 +0530

    Time: 2 ms (78.31%), Space: 7.6 MB (98.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,12 +3,13 @@ public:
     int fib(int n) {
         if(n==0 || n==1)
         return n;
-        int fibo[n+1];
-        fibo[0]=0;
-        fibo[1]=1;
+        int a=0;
+        int b=1;
         for(int i=2;i<=n;i++){
-            fibo[i]=fibo[i-1]+fibo[i-2];
+            int c=a+b;
+            a=b;
+            b=c;
         }
-        return fibo[n];
+        return b;
     }
 };
