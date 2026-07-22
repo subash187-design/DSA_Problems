5d638391842e7395389f5621d91e92d1524b51dc
Subash <s8581553@gmail.com>
2026-07-22 09:47:41 +0530

    Time: 0 ms (100%), Space: 10.7 MB (88.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,9 +14,13 @@ public:
         dp[0][j]=k;
         k++;
        }
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(word1[i-1]==word2[j-1])
+       for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(i==0)
+            dp[i][j]=j;
+            else if(j==0)
+            dp[i][j]=i;
+            else if(word1[i-1]==word2[j-1])
             dp[i][j]=dp[i-1][j-1];
             else
             dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
