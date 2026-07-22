94ab715d120f6f9f60484e16ebebb982f9e6bd74
Subash <s8581553@gmail.com>
2026-07-22 11:41:03 +0530

    Time: 38 ms (78.52%), Space: 27.3 MB (44.85%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -14,15 +14,12 @@ public:
                 dp[i][j]=0;
                 
             } 
-            else if(j==0){
-                if(s[i-1]=='*' && dp[i-1][j]==1)
-                dp[i][j]=1;
-                else 
+            else if(j==0){ 
                 dp[i][j]=0;
             }
-            else if(s[i-1]==p[j-1] || s[i-1]=='?' || p[j-1] =='?' )
+            else if(s[i-1]==p[j-1] ||  p[j-1] =='?' )
             dp[i][j]=dp[i-1][j-1];
-            else if(s[i-1]=='*' || p[j-1]=='*')
+            else if(p[j-1]=='*')
             dp[i][j]=dp[i-1][j] || dp[i][j-1];
             else
             dp[i][j]=0;
