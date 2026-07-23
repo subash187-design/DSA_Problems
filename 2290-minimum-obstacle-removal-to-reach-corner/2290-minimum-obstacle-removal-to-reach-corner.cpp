6aecafa4c3fc6e1f92647f15af2bd0557f01a8cf
Subash <s8581553@gmail.com>
2026-07-23 18:09:15 +0530

    Time: 241 ms (48.66%), Space: 108.8 MB (73.99%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -3,33 +3,28 @@ public:
     int dx[4]={-1,0,0,1};
     int dy[4]={0,-1,1,0};
     int bfs(int n,int m,vector<vector<int>>& grid){
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        deque<pair<int,pair<int,int>>>que;
-        que.push_front({0,{0,0}});
-        vis[0][0]=1;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>que;
+        que.push({0,{0,0}});
+        dist[0][0]=0;
         while(!que.empty()){
-            int w=que.front().first;
-            int i=que.front().second.first;
-            int j=que.front().second.second;
-            que.pop_front();
-            if(i==n-1 && j==m-1){
-                return w;
-            }
+            int w=que.top().first;
+            int i=que.top().second.first;
+            int j=que.top().second.second;
+            que.pop();
+            if(dist[i][j]<w) continue;
             for(int k=0;k<4;k++){
                 int x=i+dx[k];
                 int y=j+dy[k];
-                if(x>=0 && x<n && y>=0 && y<m && !vis[x][y]){
-                    vis[x][y]=1;
-                    if(grid[x][y]==1){
-                        que.push_back({w+1,{x,y}});
-                    }
-                    else{
-                        que.push_front({w,{x,y}});
+                if(x>=0 && x<n && y>=0 && y<m ){
+                    if(w+grid[x][y]<dist[x][y]){
+                    dist[x][y]=w+grid[x][y];
+                    que.push({w+grid[x][y],{x,y}});
                     }
                 }
             }
         }
-        return 0;
+        return dist[n-1][m-1];
     }
     int minimumObstacles(vector<vector<int>>& grid) {
       int n=grid.size();
