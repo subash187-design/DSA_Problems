ca78b55bc2ec01595d63d5d4ccf95f8b47318ae5
Subash <s8581553@gmail.com>
2026-07-23 15:35:31 +0530

    Time: 46 ms (36.94%), Space: 53.1 MB (10.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -7,7 +7,7 @@ public:
             dp[0][i]=1e9;
           }
           for(int i=1;i<=n;i++){
-              for(int j=0;j<=amt;j++){
+              for(int j=1;j<=amt;j++){
                   if(j<coins[i-1]) 
                   dp[i][j]=dp[i-1][j];
                   else 
