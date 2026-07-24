160571b60a2ebffea7ac3e28e55ad3b147d4c70b
Subash <s8581553@gmail.com>
2026-07-24 09:24:19 +0530

    Time: 0 ms (100%), Space: 31.2 MB (99.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,7 +4,7 @@ public:
        int l=0;
        int r=nums.size()-1;
        while(l<=r) {
-        int m=(l+r)/2;
+        int m=l+(r-l)/2;
         if(nums[m]==target) return m;
         else if(nums[m]>target) r=m-1;
         else l=m+1;
