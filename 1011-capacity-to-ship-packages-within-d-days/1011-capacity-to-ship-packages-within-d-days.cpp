3e613107fe91dfa9b323979b3f8babb1f0094c48
Subash <s8581553@gmail.com>
2026-07-24 14:26:42 +0530

    Time: 4 ms (97.01%), Space: 35.2 MB (19.73%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    bool check(int j,vector<int>& weights, int days){
+    bool check(int m,vector<int>& weights, int days){
         int sum=0;
         int cnt=1;
-        for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
-            if(sum>j){
+        for(int i:weights){
+            sum+=i;
+            if(sum>m){
                 cnt++;
-                sum=weights[i];
+                sum=i;
             }
         }
         return cnt<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int l=*max_element(weights.begin(),weights.end());
-        int r=accumulate(weights.begin(),weights.end(),0);
-        int ans=0;
-        while(l<=r){
-          int m=l+(r-l)/2;
-          if(check(m,weights,days))
-          {
+      int l=*max_element(weights.begin(),weights.end());
+      int r=accumulate(weights.begin(),weights.end(),0);
+      int ans=0;
+      while(l<=r){
+        int m=l+(r-l)/2;
+        if(check(m,weights,days)){
             ans=m;
             r=m-1;
-          }
-          else
-          {
-            l=m+1;
-          }
         }
-        return ans;
+        else
+        l=m+1;
+      }  
+      return ans;
     }
 };
