843e8692fc8b946c8a16e93d94e73224fc2d360e
Subash <s8581553@gmail.com>
2026-07-26 21:46:30 +0530

    Time: 1562 ms (5.04%), Space: 26.9 MB (46.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-       int n=s.size();
-       int m=p.size();
-       int dp[n+1][m+1];
-       for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
-            if(i==0 && j==0) dp[i][j]=1;
-            else if(i==0){
-                if(p[j-1]=='*' && dp[i][j-1]==1)
-                dp[i][j]=1;
-                else
-                dp[i][j]=0;
-                
-            } 
-            else if(j==0){ 
-                dp[i][j]=0;
+    int dp[2005][2005];
+    int n,m;
+    int rec(int i,int j,string& s,string& p){
+        if(i<0 && j<0) return 1;
+        if(i<0 && j>=0){
+            for(int k=j;k>=0;k--){
+                if(p[k]!='*') return 0;
             }
-            else if(s[i-1]==p[j-1] ||  p[j-1] =='?' )
-            dp[i][j]=dp[i-1][j-1];
-            else if(p[j-1]=='*')
-            dp[i][j]=dp[i-1][j] || dp[i][j-1];
-            else
-            dp[i][j]=0;
+            return 1;
         }
-       } 
-       return dp[n][m];
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==p[j] || p[j]=='?')
+        return dp[i][j]=rec(i-1,j-1,s,p);
+        else if(p[j]=='*')
+        return dp[i][j]=rec(i-1,j,s,p) || rec(i,j-1,s,p);
+        return dp[i][j]=0;
+    }
+    bool isMatch(string s, string p) {
+     n=s.size();
+     m=p.size();
+     memset(dp,-1,sizeof(dp));
+     return rec(n-1,m-1,s,p);   
     }
 };
