c86ca4d304ed5e7f110a8acf32567c0e3a6bf947
Subash <s8581553@gmail.com>
2026-07-29 11:05:29 +0530

    Time: 3 ms (96.79%), Space: 33 MB (61.51%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -21,9 +21,30 @@
  */
 class Solution {
 public:
-    TreeNode* mid(ListNode* head){
-        if(head==NULL) return NULL;
-        if(head->next==NULL)
+    // TreeNode* mid(ListNode* head){
+    //     if(head==NULL) return NULL;
+    //     if(head->next==NULL)
+    //     return new TreeNode(head->val);
+    //     ListNode* slow=head;
+    //     ListNode* fast=head;
+    //     ListNode* prev=NULL;
+    //     while(fast!=NULL && fast->next!=NULL){
+    //         prev=slow;
+    //         slow=slow->next;
+    //         fast=fast->next->next;
+    //     }
+    //     if(prev!=NULL)
+    //     prev->next=NULL;
+    //     TreeNode* root=new TreeNode(slow->val);
+    //     root->left=mid(head);
+    //     root->right=mid(slow->next);
+    //     return root;
+
+    // }
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head==NULL)
+        return NULL;
+      if(head->next==NULL)
         return new TreeNode(head->val);
         ListNode* slow=head;
         ListNode* fast=head;
@@ -36,15 +57,8 @@ public:
         if(prev!=NULL)
         prev->next=NULL;
         TreeNode* root=new TreeNode(slow->val);
-        root->left=mid(head);
-        root->right=mid(slow->next);
-        return root;
-
-    }
-    TreeNode* sortedListToBST(ListNode* head) {
-        if(head==NULL)
-        return NULL;
-      ListNode* temp=head;
-      return mid(temp);  
+        root->left=sortedListToBST(head);
+        root->right=sortedListToBST(slow->next);
+        return root;  
     }
 };
