095be7913859198106659a389925643cee634ac1
Subash <s8581553@gmail.com>
2026-07-29 22:59:35 +0530

    Time: 0 ms (100%), Space: 14.7 MB (96.1%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,34 +11,21 @@
  */
 class Solution {
 public:
-    vector<vector<int>>res;
-    void levelOrder(TreeNode* root) {
-       queue<pair<TreeNode*,int>>que;
-       que.push({root,0});
-       while(!que.empty()){
-          TreeNode* temp=que.front().first;
-          int k=que.front().second;
-          que.pop();
-          if (k >= res.size()) {
-              res.push_back({}); 
-          }
-          res[k].push_back(temp->val);
-         
-          if(temp->left!=NULL)
-            que.push({temp->left,k+1});
-          if(temp->right!=NULL)
-            que.push({temp->right,k+1});
-       } 
+    vector<int>res;
+    void preorder(TreeNode* root ,int lvl){
+        if(root==NULL){
+            return;
+        }
+        if(lvl==res.size())
+        res.push_back(root->val);
+        preorder(root->right,lvl+1);
+        preorder(root->left,lvl+1);
     }
     vector<int> rightSideView(TreeNode* root) {
       if(root==NULL)
       return {};
-      levelOrder(root);
-      vector<int>ans;
-      for(int i=0;i<res.size();i++){
-        int k=res[i].size();
-        ans.push_back(res[i][k-1]);
-      }
-      return ans;
+      preorder(root,0);
+      return res;
+
     }
 };
