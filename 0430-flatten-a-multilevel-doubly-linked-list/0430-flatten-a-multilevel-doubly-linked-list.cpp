1ad45384a93ba814f583ca67ddf7f51819fbd4a3
Subash <s8581553@gmail.com>
2026-07-29 14:38:03 +0530

    Time: 3 ms (63.18%), Space: 12.2 MB (10.26%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,20 +11,37 @@ public:
 
 class Solution {
 public:
-    Node* prev=NULL;
+    Node* rec(Node* head){
+        Node* temp=head;
+        Node* res=new Node(0);
+        Node* ans=res;
+        while(temp!=NULL){
+           Node* a=new Node(temp->val);
+           ans->next=a;
+           a->prev=ans;
+           ans=ans->next;
+           if(temp->child!=NULL){
+             Node *k=rec(temp->child);
+             ans->next=k;
+             k->prev=ans;
+             Node* tp1=res;
+             while(tp1->next!=NULL){
+                tp1=tp1->next;
+             }
+             ans=tp1;
+           } 
+           temp->child=NULL;
+           temp=temp->next;
+        }
+        Node* k=res->next;
+        k->prev=NULL;
+        return k;
+    }
     Node* flatten(Node* head) {
-     if(head==NULL)
-     return NULL;
-     flatten(head->next);
-     flatten(head->child);
-
-     if(prev!=NULL){
-        prev->prev=head;
-     }
-     head->child=NULL;
-     head->next=prev;
-     prev=head;
-     return head;
-
+      if(head==NULL)
+      return NULL;
+      Node* res=rec(head);
+      Node* temp=res;
+      return  res; 
     }
 };
