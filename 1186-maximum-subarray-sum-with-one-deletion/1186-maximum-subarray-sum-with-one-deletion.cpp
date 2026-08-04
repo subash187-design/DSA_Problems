43f8ced7765a3a04573bbe761b1e045bb1930530
Subash <s8581553@gmail.com>
2026-08-04 14:04:23 +0530

    Time: 3 ms (20.77%), Space: 28.8 MB (13.16%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -11,22 +11,12 @@ public:
       for(int i=n-2;i>=0;i--){
         suff[i]=max(suff[i+1]+arr[i],arr[i]);
       }  
+
       int res=INT_MIN;
-      for(int i=0;i<n;i++){
-        if(i==0){
-        if(i+1<n)
-        res=max({res,arr[i],suff[i+1]});
-        else
-        res=max(res,arr[i]);
-        }
-        else if(i==n-1){
-        if(i-1>=0)
-        res=max(res,arr[i]);
-        else
-        res=max({res,arr[i],pre[i-1]});
-        }
-        else
-        res=max({res,pre[i-1]+suff[i+1],pre[i],suff[i],pre[i]+suff[i+1]});
+      for(int i:pre)
+      res=max(res,i);
+      for(int i=1;i<n-1;i++){
+        res=max(res,pre[i-1]+suff[i+1]);
       }
       return res;
     }
