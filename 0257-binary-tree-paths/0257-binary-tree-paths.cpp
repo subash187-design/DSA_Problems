8872cf3c8055c8197c0fb134eb7bb17cfa4016af
Subash <s8581553@gmail.com>
2026-08-05 12:32:15 +0530

    Time: 0 ms (100%), Space: 18.2 MB (13.63%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -13,31 +13,32 @@ class Solution {
 public:
     vector<string>res;
     void rec(TreeNode* root,string s){
-        if(root==NULL){
-            return ;
-        }
-        s+="->";
+        if(root==NULL)
+        return;
+        if(s=="")
         s+=to_string(root->val);
-        if(root->left==NULL && root->right==NULL){
-         res.push_back(s);
-            s="";
+        else{
+        s=s+'-'+'>';
+        s+=to_string(root->val);
+        }
+        if(root->left==NULL && root->right==NULL)
+        {
+            res.push_back(s);
+            return ;
         }
         rec(root->left,s);
         rec(root->right,s);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
-    if(root==NULL)
-    return {};
-    string s=to_string(root->val);
-    if(root->left==NULL && root->right==NULL){
-         res.push_back(s);
-            s="";
-            return res;
-    }
-    rec(root->left,s);
-    s="";
-    s+=to_string(root->val);
-    rec(root->right,s);
-    return res;
+        if(root==NULL)
+        return {};
+        string s="";
+        if(root->left==NULL && root->right==NULL){
+        s+=to_string(root->val);
+        res.push_back(s);
+        return res;
+        }
+        rec(root,s);
+        return res;
     }
 };
