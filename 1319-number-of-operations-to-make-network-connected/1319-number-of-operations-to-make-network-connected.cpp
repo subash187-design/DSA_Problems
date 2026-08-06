dc6b84ceafe6bd0a3f6104c9f0902932484f91ce
Subash <s8581553@gmail.com>
2026-08-06 11:07:09 +0530

    Time: 48 ms (9.25%), Space: 72.5 MB (5.05%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    int bfs(int i,int n,vector<vector<int>>&adj,vector<int>&vis){
+    void bfs(int i,int n,vector<vector<int>>&adj,vector<int>&vis){
        queue<int>que;
        que.push({i});
        vis[i]=1;
-       int c=0;
        while(!que.empty()){
         int a=que.front();
-        c++;
         que.pop();
         for(int i:adj[a]){
             if(vis[i]==0)
@@ -17,14 +15,11 @@ public:
             }
         }
        }
-       if(c==1)
-       return 0;
-       return c;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-    int k=connections.size();
-      if((n-1)>k)
-     return -1;
+       int k=connections.size();
+       if((n-1)>k)
+       return -1;
        vector<vector<int>>adj(n);
        vector<int>vis(n,0);
        for(int i=0;i<connections.size();i++){
@@ -36,8 +31,7 @@ public:
        int cnt=0;
        for(int i=0;i<n;i++){
         if(!vis[i]){
-            int a=bfs(i,n,adj,vis);
-            cout<<a<<endl;
+            bfs(i,n,adj,vis);
             cnt++;
         }
        }
