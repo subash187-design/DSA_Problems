ca47004da16e79f563ff347af70eaa09ca7a0aa6
Subash <s8581553@gmail.com>
2026-08-08 09:53:40 +0530

    Time: 0 ms (100%), Space: 17.5 MB (78.8%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -13,13 +13,11 @@ class Solution {
 public:
     TreeNode* prev=NULL;
     void flatten(TreeNode* root) {
-      if(root==NULL)
-      return;
-      flatten(root->right);
-      flatten(root->left);
-      root->right=prev;
-      root->left=NULL; 
-      prev=root;
-
+        if(root==NULL) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
     }
 };
