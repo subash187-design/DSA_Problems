4ded5c1f5f433c4547237fae91bc3ce79f77ce5d
Subash <s8581553@gmail.com>
2026-08-12 14:04:34 +0530

    Time: 697 ms (23.66%), Space: 56.1 MB (69.53%) - LeetHub

---
+++ 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[2004][52][52];
+    int mod=1e9+7;
+    int rec(int i,int prevInc,int prevDec,vector<int>&nums){
+        if(i>=nums.size())
+        return 1;
+        if(dp[i][prevInc][prevDec]!=-1)
+        return dp[i][prevInc][prevDec];
+        int ans=0;
+        int n=nums[i];
+        for(int j=prevInc;j<=n;j++){
+            if(nums[i]-j<=prevDec){
+                ans=(ans+rec(i+1,j,nums[i]-j,nums))%mod;
+            }
+        }
+        return dp[i][prevInc][prevDec]=ans;
+    }
+    int countOfPairs(vector<int>& nums) {
+      memset(dp,-1,sizeof(dp));
+      return rec(0,0,51,nums);  
+    }
+};
