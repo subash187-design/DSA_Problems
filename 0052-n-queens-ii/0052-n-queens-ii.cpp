27b54da3e5ebdee25ace9dff10251e1ed3458fe6
Subash <s8581553@gmail.com>
2026-08-14 09:52:16 +0530

    Time: 2 ms (55.23%), Space: 8.1 MB (63.02%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -2,42 +2,22 @@ class Solution {
     int res=0;
 
 public:
-    bool check(int row,int col,int n,vector<string>&temp){
-        int origRow=row;
-        int origCol=col;
-        while(row>=0 && col>=0){
-            if(temp[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-        row=origRow;
-        col=origCol;
-        while(row<n && col>=0){
-            if(temp[row][col]=='Q') return false;
-            
-            row++;
-            col--;
-        }
-        row=origRow;
-        col=origCol;
-        while(col>=0){
-            if(temp[row][col]=='Q') return false;
-            col--;
-        
-       }
-       return true;
-
-    }
-    void solve(int i, int n, vector<string>& temp) {
+    void solve(int i, int n, vector<string>& temp,vector<int>&row,vector<int>&upper,vector<int>&lower) {
         if (i == n) {
             res++;
             return;
         }
         for(int j=0;j<n;j++){
-            if(check(j,i,n,temp)){
+            if(lower[i+j]==0 && row[j]==0 && upper[(j-i)+(n-1)]==0){
                 temp[j][i]='Q';
-                solve(i+1,n,temp);
+                lower[i+j]=1;
+                upper[(j-i)+(n-1)]=1;
+                row[j]=1;
+                solve(i+1,n,temp,row,upper,lower);
                 temp[j][i]='.';
+                lower[i+j]=0;
+                upper[(j-i)+(n-1)]=0;
+                row[j]=0;
             }
         }
     }
@@ -50,7 +30,10 @@ public:
             }
             temp.push_back(a);
         }
-        solve(0, n, temp);
+        vector<int>row(n,0);
+        vector<int>upper((1<<n)-1,0);
+        vector<int>lower((1<<n)-1,0);
+        solve(0, n, temp,row,upper,lower);
         return res;
     }
 };
