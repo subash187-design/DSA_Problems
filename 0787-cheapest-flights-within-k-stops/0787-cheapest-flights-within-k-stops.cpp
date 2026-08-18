ce0597d8fd0fcdf43647e9fa60d8ca808af84925
Subash <s8581553@gmail.com>
2026-08-18 10:06:38 +0530

    Time: 4 ms (46.03%), Space: 17.7 MB (73.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,56 +1,43 @@
 class Solution {
 public:
-    int dijkstra(int n, vector<vector<pair<int, int>>>& adj, int src, int dst,
-                 int k) {
-
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-
-        set<pair<int, pair<int, int>>> st;
-
-
-        dist[src][k + 1] = 0;
-        st.insert({0, {src, k + 1}});
-
-        while (!st.empty()) {
-
-            auto it = *st.begin();
+    int res=INT_MAX;
+    int dijkstra(int n, vector<vector<pair<int,int>>>& adj, int src, int dst, int k){
+        vector<int>dist(n,INT_MAX);
+        set<pair<int,pair<int,int>>>st;
+        st.insert({0,{0,src}});
+        while(!st.empty()){
+            auto it=*st.begin();
+            int x=it.first;
+            int dis=it.second.first;
+            int node=it.second.second;
+           
             st.erase(st.begin());
-
-            int dis = it.first;
-            int node = it.second.first;
-            int rem = it.second.second;
-
-            if (node == dst)
-                return dis;
-
-            if (dis > dist[node][rem])
+            for(auto i:adj[node]){
+                int nod=i.first;
+                int wt=i.second;
+                if(nod==dst){
+                    res=min(res,dis+wt);
+                }
+                else if(x>=k)
                 continue;
-
-            for (auto& i : adj[node]) {
-
-                int nod = i.first;
-                int wt = i.second;
-
-                if (rem > 0 && dis + wt < dist[nod][rem - 1]) {
-
-                    dist[nod][rem - 1] = dis + wt;
-
-                    st.insert({dist[nod][rem - 1], {nod, rem - 1}});
+                else if(dis+wt<dist[nod]){
+                    dist[nod]=dis+wt;
+                    st.insert({x+1,{dist[nod],nod}});
                 }
             }
+           
         }
-
-        return -1;
+         return res;
     }
-
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-
-        vector<vector<pair<int, int>>> adj(n);
-
-        for (auto& i : flights)
-            adj[i[0]].push_back({i[1], i[2]});
-
-        return dijkstra(n, adj, src, dst, k);
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(int i=0;i<flights.size();i++){
+            int a=flights[i][0];
+            int b=flights[i][1];
+            int c=flights[i][2];
+            adj[a].push_back({b,c});
+        }
+        dijkstra(n,adj,src,dst,k);
+        return res==INT_MAX?-1:res;
     }
 };
