65d8ca8b982e2ceaffa05b7571be0b8502220385
Subash <s8581553@gmail.com>
2026-08-21 17:47:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.76%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int dp[24][24];
-    int n,m;
-    int rec(int i,int j,string &s,string &p){
-        if(i==n && j==m) return 1;
-        if(i>n || j>m ) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        if(s[i]==p[j] || p[j]=='.')
-        ans=ans || rec(i+1,j+1,s,p);
-        if(j+1<m && p[j+1]=='*'){
-            if(s[i]==p[j] || p[j]=='.'){
-                ans=ans || rec(i+1,j,s,p);
+    int dp[22][22];
+    int rec(int i,int j,string& s,string& p){
+        if(i<0){
+            for(int k=j;j>=0;j-=2){
+            if(p[j]!='*') return 0;
             }
-              ans=ans || rec(i,j+2,s,p);
+            return 1;
         }
-        cout<<i<<" "<<j<<" "<<ans<<endl;
+        if(j<0) 
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int ans = 0;
+        if(s[i]==p[j] || p[j]=='.')
+        ans = ans || rec(i-1,j-1,s,p);
+        if(p[j]=='*'){
+        if(j-1>=0 && s[i]==p[j-1] || p[j-1]=='.')
+        ans = ans || rec(i-1,j,s,p) || rec(i-1,j-2,s,p);
+        ans = ans || rec(i,j-2,s,p);
+        }
         return dp[i][j]=ans;
     }
     bool isMatch(string s, string p) {
-        n=s.size();
-        m=p.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0,s,p);
+        int n=s.size();
+        int m=p.size();
+        return  rec(n-1,m-1,s,p);
     }
 };
