14008693ce3bdcfe2d4fb5da964ee6296cc79d68
Subash <s8581553@gmail.com>
2026-08-21 17:17:34 +0530

    Time: 1300 ms (5%), Space: 26.9 MB (47.29%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int dp[2005][2005];
-    int n,m;
+    int dp[2004][2004];
     int rec(int i,int j,string& s,string& p){
-        if(i<0 && j<0) return 1;
-        if(i<0 && j>=0){
+        if(i<0){
             for(int k=j;k>=0;k--){
-                if(p[k]!='*') return 0;
+                if(p[k]!='*')
+                return 0;
             }
             return 1;
         }
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(j<0)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int ans=0;
         if(s[i]==p[j] || p[j]=='?')
-        return dp[i][j]=rec(i-1,j-1,s,p);
+        ans=ans || rec(i-1,j-1,s,p);
         else if(p[j]=='*')
-        return dp[i][j]=rec(i-1,j,s,p) || rec(i,j-1,s,p);
-        return dp[i][j]=0;
+        ans = ans || rec(i-1,j,s,p) || rec(i,j-1,s,p);
+        return dp[i][j]=ans;
+
     }
     bool isMatch(string s, string p) {
-     n=s.size();
-     m=p.size();
-     memset(dp,-1,sizeof(dp));
-     return rec(n-1,m-1,s,p);   
+        memset(dp,-1,sizeof(dp));
+        int n=s.size();
+        int m=p.size();
+        return rec(n-1,m-1,s,p);
+        
     }
 };
